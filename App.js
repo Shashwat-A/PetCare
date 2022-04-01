ad0084f0166a3242763f703eb79dd6c3b916879b
Shashwat-A <sendmail2ruchi@gmil.com>
2022-04-01 17:44:48 +0530

    stage 1, btns nav, new colors

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import StackNavigator from './component/StackNavigator';
+
+export default function App() {
+  return (
+    <StackNavigator/>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
